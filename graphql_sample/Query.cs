acc85894c70d42ac8fe95e5acd8d978de9d59333
610engineer <m_muto@jour-1.com>
2023-02-18 16:01:18 +0900

    first commit

---
+++ graphql_sample/Query.cs
@@ -0,0 +1,15 @@
+﻿namespace graphql_sample
+{
+    public class Query
+    {
+        public book GetBook() =>
+            new book
+            {
+                title = "GRAPHQL",
+                Author = new Author
+                {
+                    Name = "Sample"
+                }
+            };
+    }
+}
---
+++ graphql_sample/book.cs
@@ -0,0 +1,13 @@
+﻿namespace graphql_sample
+{
+    public class book
+    {
+        public string title { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+}
